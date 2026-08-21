d9421d830ff8a66e503e5d71cbd446f3bbbc48f5
Anurag Yadav <anuragvnm01@gmail.com>
2026-08-21 19:42:29 +0530

    initial commit

---
+++ Backtraking/ArrayLIst.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class ArrayLIst {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+        ArrayList<Integer> list3 = new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.set(2,10);
+        System.err.println(list);
+
+        
+    }
+    
+}
--- Backtraking/Gridways.java
+++ Backtraking/Gridways.java
@@ -1,22 +1,29 @@
 public class Gridways {
-    public static int gridWays(int i , int j, int n, int m){
-        if(i==n-1&& j==m-1){
+
+    public static int gridWays(int i, int j, int n, int m) {
+
+        // Destination cell
+        if (i == n - 1 && j == m - 1) {
             return 1;
         }
-            else if(i==n || j==n){
-                return 0;
-                }
-                int  w1 = gridWays(i+1, j, n, m);
-                int  w2 = gridWays(i, j+1, n, m);
-                return w1 + w2;
 
-        
+        // Boundary cross
+        if (i == n || j == m) {
+            return 0;
+        }
 
+        // Move down
+        int w1 = gridWays(i + 1, j, n, m);
+
+        // Move right
+        int w2 = gridWays(i, j + 1, n, m);
+
+        return w1 + w2;
     }
 
     public static void main(String[] args) {
-        int n = 3 , m = 3;
-        System.err.println(gridWays(0,0,n,m));
+        int n = 55,m =55;
+
+        System.out.println(gridWays(0, 0, n, m));
     }
-    
 }
